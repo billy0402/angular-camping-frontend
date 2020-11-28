aa4dfa7c0189e6302fa89c14848d907f8939f3fc
Billy Huang <10646003@ntub.edu.tw>
2020-11-28 18:00:56 +0800

    feat(app): app finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('@pages/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'product',
+    loadChildren: () =>
+      import('@pages/product/product.module').then((m) => m.ProductModule),
+  },
+  {
+    path: 'borrow',
+    loadChildren: () =>
+      import('@pages/borrow/borrow.module').then((m) => m.BorrowModule),
+  },
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('@pages/user/user.module').then((m) => m.UserModule),
+  },
+  {
+    path: 'faq',
+    loadChildren: () =>
+      import('@pages/faq/faq.module').then((m) => m.FaqModule),
+  },
+
+  { path: '', redirectTo: '/product', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,62 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ImageCropperModule } from 'ngx-image-cropper';
+import { NgImageSliderModule } from 'ng-image-slider';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { JwtModule } from '@auth0/angular-jwt';
+
+import { MaterialModule } from '@modules/material.module';
+
+import { ComponentsModule } from '@components/components.module';
+
+import { CustomHttpInterceptor } from '@interceptors/custom-http.interceptor';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    // Angular modules
+    BrowserAnimationsModule,
     BrowserModule,
+    HttpClientModule,
+
+    // Third party modules
+    FlexLayoutModule,
+    ImageCropperModule,
+    NgImageSliderModule,
+    NgxPaginationModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: () => {
+          return localStorage.getItem('access_token');
+        },
+        allowedDomains: ['211.75.1.201:50004'],
+      },
+    }),
+
+    // Shared modules
+    MaterialModule,
+
+    // Components modules
+    ComponentsModule,
+
+    // App's modules
     AppRoutingModule,
-    BrowserAnimationsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CustomHttpInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
